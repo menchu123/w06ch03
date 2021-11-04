c36e4ff597808ab1b6fa52c321947752cdf7ab18
Menchu MV <mcmvillalobos@gmail.com>
2021-11-04 21:35:02 +0100

    Added inquirer questions and condition in database

--- database/index.js
+++ database/index.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const debug = require("debug")("cosas:database");
 
-mongoose.connect(process.env.MONGODB_STRING, (error) => {
-  if (error) {
-    debug(chalk.red("No se ha podido iniciar la base de datos."));
-    debug(chalk.red(error.message));
-    return;
-  }
-  debug(chalk.green("Conectado a la base de datos"));
-});
+const dataBaseProduction = process.env.MONGODB_STRING;
+const dataBasePruebas = process.env.MONGODB_STRING_PRUEBAS;
+
+const dataBase = (dataBaseString) => {
+  mongoose.connect(
+    dataBaseString === "produccion" ? dataBaseProduction : dataBasePruebas,
+    (error) => {
+      if (dataBaseString === "produccion") {
+        debug(chalk.green("tamo produsiendo"));
+      } else if (dataBaseString === "pruebas") {
+        debug(chalk.green("tamo probando"));
+      }
+      if (error) {
+        debug(chalk.red("La db no funksiona"));
+        debug(chalk.red(error.message));
+      }
+    }
+  );
+};
+
+module.exports = dataBase;
