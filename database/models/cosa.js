b38335cbc0e0955d014f965b2e4908321270637c
Menchu MV <mcmvillalobos@gmail.com>
2021-11-04 20:28:54 +0100

    Added cosa schema

--- database/models/cosa.js
+++ database/models/cosa.js
@@ -1,1 +1,12 @@
+const { Schema, model } = require("mongoose");
 
+const cosaSchema = new Schema({
+  cosa: {
+    type: String,
+    required: true,
+  },
+});
+
+const Cosa = model("Cosa", cosaSchema);
+
+module.exports = Cosa;
