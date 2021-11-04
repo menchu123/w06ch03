69cbc57f663590eb4a13a30bc98276238ee8aa00
Menchu MV <mcmvillalobos@gmail.com>
2021-11-04 18:56:49 +0100

    Created folder structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const chalk = require("chalk");
+const debug = require("debug")("cosas:server");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const initializeServer = (port) => {
+  const server = app.listen(port, () => {
+    debug(chalk.yellow(`Escuchando en el puerto ${port}`));
+  });
+
+  server.on("error", (error) => {
+    debug(chalk.red("Ha habido un error al iniciar el servidor."));
+    if (error.code === "EADDRINUSE") {
+      debug(chalk.red(`El puerto ${port} está en uso.`));
+    }
+  });
+};
+
+module.exports(initializeServer);
